6b863aec125dd25c95c63a759e5f43c030474b85
Clark Grubb <clarkgrubb@gmail.com>
2013-10-02 09:52:15 -0700

    shorten data file names; pass output files to generate.rb

--- Makefile
+++ Makefile
@@ -1,5 +1,21 @@
-all:
-	./generate.rb
+MAKEFLAGS += --warn-undefined-variables
+SHELL := /bin/bash
+.SHELLFLAGS := -o pipefail
+.DEFAULT_GOAL := all
+.DELETE_ON_ERROR:
+.SUFFIXES:
+
+data_dir := data
+data_files := $(wildcard $(data_dir)/*)
+targets := latex.el latex.cin
+
+latex.el: $(data_files)
+	./generate.rb --emacs-lisp-file=$@
+
+latex.cin: $(data_files)
+	./generate.rb --cin-file=$@
+
+all: $(targets)
 
 clean:
-	-rm -rf build
+	rm $(targets)
--- generate.rb
+++ generate.rb
@@ -2,52 +2,62 @@
 
 require 'erb'
 require 'fileutils'
+require 'optparse'
 
 ROOT_DIR = File.dirname(__FILE__)
 DATA_DIR = File.join(ROOT_DIR, 'data')
 TEMPLATES_DIR = File.join(ROOT_DIR, 'templates')
-BUILD_DIR = ROOT_DIR
-
-LATEX_FILES = %w(
-  latex_to_unicode.txt
-  latex_to_unicode_english_punct.txt
-  latex_to_unicode_latin_accent.txt
-  latex_to_unicode_greek.txt
-  latex_to_unicode_applekeys.txt
-  latex_to_unicode_mathfont.txt
-  latex_to_unicode_not.txt
-  latex_to_unicode_subscripts.txt
-  latex_to_unicode_superscripts.txt
-)
-
-LATEX_TO_UNICODE_FILES = LATEX_FILES.map { |lf| File.join(DATA_DIR, lf) }
-
-FileUtils.mkdir_p(BUILD_DIR)
-
-LATEX_CIN = File.join(BUILD_DIR, 'latex.cin')
 LATEX_CIN_TEMPLATE = File.join(TEMPLATES_DIR, 'latex.cin.erb')
-
-LATEX_EL = File.join(BUILD_DIR, 'latex.el')
 LATEX_EL_TEMPLATE = File.join(TEMPLATES_DIR, 'latex.el.erb')
 
 def escape(str)
   str.gsub(/\\/, '\\\\\\\\').gsub(/"/, '\\\\"')
 end
 
-latex_to_unicode = {}
+def load_data(data_dir)
+  latex_to_unicode = {}
+  latex_to_unicode_files = Dir.glob("#{data_dir}/*")
+  latex_to_unicode_files.each do |path|
+    File.open(path).each do |line|
+      fields = line.strip.split()
+      if fields.size != 2
+        raise Exception('expected two fields on line: #{line}')
+      end
+      latex_to_unicode[fields[0]] = fields[1]
+    end
+  end
+  latex_to_unicode
+end
 
-LATEX_TO_UNICODE_FILES.each do |latex_to_unicode_file|
-  File.open(latex_to_unicode_file).each do |line|
-    fields = line.strip.split()
-    raise Exception('expected two fields on line: #{line}') if fields.size != 2
-    latex_to_unicode[fields[0]] = fields[1]
+def generate(latex_el, latex_cin)
+  latex_to_unicode = load_data(DATA_DIR)
+
+  if (latex_el)
+    el_template = ERB.new(File.open(LATEX_EL_TEMPLATE).read)
+    File.open(latex_el, 'w') { |fout| fout.puts el_template.result(binding) }
+  end
+
+  if (latex_cin)
+    cin_template = ERB.new(File.open(LATEX_CIN_TEMPLATE).read)
+    File.open(latex_cin, 'w') { |fout| fout.puts cin_template.result(binding) }
   end
 end
 
-cin_template = ERB.new(File.open(LATEX_CIN_TEMPLATE).read)
+if $0 == __FILE__
 
-File.open(LATEX_CIN, 'w') { |fout| fout.puts cin_template.result(binding) }
+  options = {}
+  OptionParser.new do |opts|
+    opts.banner =
+      "usage: #{$0} [OPTIONS] [ARG ...]"
 
-el_template = ERB.new(File.open(LATEX_EL_TEMPLATE).read)
+    opts.on("-e", "--emacs-lisp-file FILE") do |arg|
+      options[:emacs_lisp_file] = arg
+    end
 
-File.open(LATEX_EL, 'w') { |fout| fout.puts el_template.result(binding) }
+    opts.on("-c", "--cin-file FILE") do |arg|
+      options[:cin_file] = arg
+    end
+  end.parse!
+
+  generate(options[:emacs_lisp_file], options[:cin_file])
+end
